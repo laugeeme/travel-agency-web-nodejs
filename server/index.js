011606fcd52327a28cbcfd618edac287740770f5
laugeeme <registro.laura@gmail.com>
2020-07-16 13:45:29 +0200

    configure pug

--- server/index.js
+++ server/index.js
@@ -3,10 +3,17 @@
 
 //Import Express
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 //Configure Express
 const app = express();
+
+//Load PUG
+app.set('view engine', 'pug');
+
+//Add views
+app.set.set('views', path.join(__dirname, './views'));
 
 //Load routes
 app.use('/', routes());
