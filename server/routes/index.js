6f5373f7cc5e633d1b4bddfec7bb651f8f4eb045
laugeeme <registro.laura@gmail.com>
2020-07-16 19:14:31 +0200

    conect result from database to view

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Trip = require('../models/Trips');
+
 //here we can put all the routes form our proyect
 module.exports = function () {
   router.get('/', (req, res) => {
@@ -10,14 +12,19 @@ module.exports = function () {
 
   router.get('/nosotros', (req, res) => {
     res.render('nosotros', {
-      page: 'Sobre nosotros' //Create new variable to use in nosotros/index.pug
+      page: 'Sobre nosotros', //Create new variable to use in nosotros/index.pug
     });
   });
 
   router.get('/viajes', (req, res) => {
-    res.render('viajes', {
-      page: 'Próximos viajes' //Create new variable to use in nosotros/index.pug
-    });
+    Trip.findAll()
+      .then((trips) =>
+        res.render('viajes', {
+          page: 'Próximos Viajes',
+          trips
+        })
+      )
+      .catch((error) => console.log(error));
   });
 
   return router;
