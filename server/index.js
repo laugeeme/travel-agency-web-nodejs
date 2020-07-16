16b0c8e552e96e98b6de6b16c611a41ee660194d
laugeeme <registro.laura@gmail.com>
2020-07-16 16:35:56 +0200

    add header and footer. Set current year as a global variable

--- server/index.js
+++ server/index.js
@@ -18,6 +18,15 @@ app.set('views', path.join(__dirname, './views'));
 //Load PUBLIC (static file)
 app.use(express.static('public'));
 
+//Displays the current date
+app.use((req, res, next) => {
+  //Create new date
+  const date = new Date();
+  res.locals.dateNow = date.getFullYear(); //res.locals as a global variables
+  return next();
+});
+
+
 //Load routes
 app.use('/', routes());
 
