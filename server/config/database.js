befcca0d77a4ddbb52858f8110c9b2648b756d1e
laugeeme <registro.laura@gmail.com>
2020-07-17 17:15:07 +0200

    edit envars

--- server/config/database.js
+++ server/config/database.js
@@ -4,9 +4,9 @@ require('dotenv').config({ path: 'variables.env' });
 
 
 
-module.exports = new Sequelize(process.env.BD_NAME, process.env.BD_USER, '', {
-  host: process.env.HOST,
-  port: process.env.PORT,
+module.exports = new Sequelize(process.env.BD_NAME, process.env.BD_USER, process.env.BD_PASS, {
+  host: process.env.BD_HOST,
+  port: process.env.BD_PORT,
   dialect: 'mysql',
   define: {
     timestamps: false,
